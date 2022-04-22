ef49065c6a32708683f2f7143aefd16ee0e06f9a
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2022-04-22 13:07:23 +0200

    Update simpleServer.js

--- simpleServer.js
+++ simpleServer.js
@@ -3,6 +3,6 @@ const http = require('http');
 http.createServer(function(req, res) {
 	res.write('Hello World');
 	res.end();
-}).listen(8080);
+}).listen(3000);
 
-console.log('Server started! Listening on port 8080');
+console.log('Server started! Listening on port 3000');
